4d209a95e288efa219098f43ed337a780b67e0a9
SalixeGames <salixegames@gmail.com>
2026-01-28 19:00:28 -0500

    Revert "!! starting enemies over"
    This reverts commit 5e06db0069f1bc00e6f7c37e289e60f1c69c9d71.

---
+++ scripts/enemies/EnState.cs
@@ -0,0 +1,36 @@
+using System;
+using Godot;
+
+namespace Platformerz.scripts.enemies;
+
+[GlobalClass]
+public partial class EnState : Resource
+{
+	[Export] public string Name;
+	public EnStateMachine fsm;
+	public BaseEnemy Controller;
+
+	public virtual void Enter()
+	{
+		fsm.Controller.UpdateAnim(Name);
+	}
+	public virtual void Exit() {}
+
+	public virtual void Ready(EnStateMachine stateMachine)
+	{
+	}
+
+	public virtual EnState Update(float delta)
+	{
+		return this;
+	}
+	public virtual EnState PhysicsUpdate(float delta) 
+	{
+		return this;
+	}
+
+	public virtual EnState AnimationEnd(string animationName) 
+	{
+		return this;
+	}
+}
